432b7315f659dbb54313f28007d711fbbb55b4b5
dimka90 <dimkayilrit@gmail.com>
2024-01-29 19:19:40 +0100

    created the landing page and installed flask application

---
+++ web-flask/static/scripts/index.js
@@ -0,0 +1,20 @@
+const navigation=document.querySelector('.primary-navigation');
+const btnToggle=document.querySelector('.btn-img');  
+
+
+btnToggle.addEventListener("click",()=>{
+    const visible=navigation.getAttribute('data-visible');
+    const image=document.getElementById('harmburger-menu');
+  
+    console.log(visible);
+    if (visible==="false"){
+    
+        navigation.setAttribute('data-visible',true);
+        image.setAttribute('src','../static/images/download.jpeg');
+    }
+    else if(visible==="true"){
+        navigation.setAttribute('data-visible',false);  
+        image.setAttribute('src','../static/images/menu.png');
+    }
+
+});

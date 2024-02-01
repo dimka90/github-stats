4e57c38a0644d964b1074b0cad61e7b9f803431c
dimka90 <dimkayilrit@gmail.com>
2024-02-01 13:21:19 +0100

    fix some bug

--- web-flask/app.py
+++ web-flask/app.py
@@ -15,14 +15,32 @@ def get_github_user():
     response = requests.get(url)
     if response.status_code == 200:
         user_data = response.json()
+        avatar_url =  user_data.get('avatar_url', '')
         data = {"Username" : user_data['login'],
                 "Name": user_data["name"],
                 "Location": user_data["location"],
                 "Bio": user_data['bio'],
-                "Followers": user_data['followers']
+                "Followers": user_data['followers'],
                 }
-        return  render_template("user_info.html", data = data)
+        return  render_template("user_info.html", data = data, avatar_url = avatar_url)
     else:
         return "still processing"
+@app.route("/get_user_projects", methods =["GET"])
+def get_user_projects():
+    username = request.args.get("username")
+    # obtainning access token 
+    with open("token") as file:
+        acces_token = file.read()
+    base_url = f'https://api.github.com/users/{username}/repos'
+    headers  = {'Authorization': f'token {acces_token}'}
+
+    response = requests.get(base_url, headers = headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return "Still processing"
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="5000", debug=True)
